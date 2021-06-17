d80aaf00782abff7e18d4dcc22a6221fa106240d
PILKWANG <soulkwang@naver.com>
2021-06-18 08:53:48 +0900

    210618

---
+++ Pressure_plx/Pressure_plx.ino
@@ -0,0 +1,20 @@
+#include "VernierLib.h" //include Vernier functions in this sketch
+VernierLib Vernier; //create an instance of the VernierLib library
+int count=0;
+
+void setup() { // setup the initial condition
+Serial.begin(9600);
+Vernier.autoID(); //identify the sensor being used
+Serial.println("CLEARDATA");
+Serial.println("LABEL, TIME, time, Pressure");
+}
+
+void loop() { //code in which the valve opens when the pressure drops below 0.25atm
+count ++;
+float Pressure = Vernier.readSensor()/103.325; // convert pressure units from kPa to atm
+Serial.print("DATA, TIME, ");
+Serial.print(count*0.5); //count the time in 0.5 second intervals
+Serial.print(",");
+Serial.println(Pressure);
+delay(500);
+}
