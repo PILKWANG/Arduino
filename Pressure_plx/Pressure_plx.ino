6b4602e984e012bfa879174a6e9e2c430bac4537
PILKWANG <soulkwang@naver.com>
2021-11-24 09:49:32 +0900

    211124

--- Pressure_plx/Pressure_plx.ino
+++ Pressure_plx/Pressure_plx.ino
@@ -1,5 +1,6 @@
 #include "VernierLib.h" //include Vernier functions in this sketch
 VernierLib Vernier; //create an instance of the VernierLib library
+#define DELAYVAL 500
 int count=0;
 
 void setup() { // setup the initial condition
@@ -13,8 +14,8 @@ void loop() { //code in which the valve opens when the pressure drops below 0.25
 count ++;
 float Pressure = Vernier.readSensor()/103.325; // convert pressure units from kPa to atm
 Serial.print("DATA, TIME, ");
-Serial.print(count*0.5); //count the time in 0.5 second intervals
+Serial.print(count*DELAYVAL*0.001); //count the time in 0.5 second intervals
 Serial.print(",");
 Serial.println(Pressure);
-delay(500);
+delay(DELAYVAL);
 }
