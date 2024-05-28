5ad4919e18dc940e0e41acab8c00d2c354043b8d
unknown <soulkwang@naver.com>
2024-05-28 12:39:50 +0900

    240528

--- pH sensor/pH_plx/pH_plx.ino
+++ pH sensor/pH_plx/pH_plx.ino
@@ -1,4 +1,4 @@
-#define pHsensor 0
+#define SensorPin 0
 #define OFFSET 0.00
 
 int count=0;
@@ -10,15 +10,30 @@ void setup() {
  }
 
 void loop() {
-
-count ++;
-
-int sensorvalue = analogRead(pHsensor);
-float voltage=(float)sensorvalue*0.5/1024;
-float pHvalue=(voltage*35)+OFFSET;
-Serial.print("DATA,TIME,");
-Serial.print (count);
-Serial.print(",");
-Serial.println(pHvalue,2);
-delay(500);
+  for(int i=0;i<10;i++)
+  { 
+    buf[i]=analogRead(SensorPin);
+    delay(10);
+  }
+  for(int i=0;i<9;i++)
+  {
+    for(int j=i+1;j<10;j++)
+    {
+      if(buf[i]>buf[j])
+       {
+        temp=buf[i];
+        buf[i]=buf[j];
+        buf[j]=temp;
+      }}}
+  count ++;      
+  avgValue=0;
+  for(int i=2;i<8;i++)
+    avgValue+=buf[i];
+  float pHvalue=(float)avgValue*5.0/1024/6;
+  pHvalue=3.5*pHvalue+OFFSET;  
+  Serial.print("DATA, TIME, ");
+  Serial.print(count*1);
+  Serial.print(",");
+  Serial.println(pHvalue);
+  delay(500);
 }
